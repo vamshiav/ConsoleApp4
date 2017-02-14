f3ecf4bfb7798eea3d1154d3132471d9b2a97860
vamshiav <vamshi.avadhanula@gmail.com>
2017-02-14 16:38:31 -0500

    initial 55

---
+++ src/ConsoleApp4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            //int[] numbers = { 8, 2, 6, 3 };
+            //var sum = numbers.Sum();
+            //Console.WriteLine(sum);
+
+
+            //int[] numbers = { 8, 2, 6, 3 };
+            //var max = numbers.Max();
+            //Console.WriteLine(max);
+
+            //int[] numbers = { 8, 2, 6, 3 };
+            //var min = numbers.Min();
+            //Console.WriteLine(min);
+
+            //string[] letters = { "A", "B", "C", "D" };
+
+            //var result = letters.Count();
+
+            //Console.WriteLine(result);
+
+
+            //int[] numbers = { 8, 2, 6, 3 };
+            
+            //var result = numbers.Average();
+
+            //Console.WriteLine(result);
+
+
+
+        }
+    }
+}
---
+++ src/ConsoleApp4/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConsoleApp4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Olympus Corporation of the Americas")]
+[assembly: AssemblyProduct("ConsoleApp4")]
+[assembly: AssemblyCopyright("Copyright © Olympus Corporation of the Americas 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0a13aaad-42c7-4d8b-bb7b-49a98b6ada0f")]
